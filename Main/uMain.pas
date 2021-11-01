37e15dbf9b93745668f9819dd73409d1f95585bb
mateuslph <mlptche@gmail.com>
2021-11-01 14:48:03 -0300

    adicionado edits, e botao

--- Main/uMain.pas
+++ Main/uMain.pas
@@ -4,11 +4,15 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage,
+  Vcl.StdCtrls;
 
 type
   TfrmMain = class(TForm)
     imLogin: TImage;
+    edtUser: TEdit;
+    edtPass: TEdit;
+    btnLogin: TButton;
   private
     { Private declarations }
   public
